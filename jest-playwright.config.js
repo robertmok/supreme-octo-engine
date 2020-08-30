f2758fc5ccae62b9ac0b0684566d3f4025226ea9
Robert <robert.y.mok@gmail.com>
2020-08-30 01:35:32 -0400

    jest playwright preset

---
+++ jest-playwright.config.js
@@ -0,0 +1,60 @@
+module.exports = {
+    launchOptions: {
+      headless: true
+    },
+    contextOptions: {
+      ignoreHTTPSErrors: true,
+      viewport: {
+        width: 1920,
+        height: 1080
+      }
+    },
+    browsers: ["chromium", "firefox", "webkit"],
+    devices: [
+      {
+        // Name of device
+        name: '1440',
+        // Page width and height
+        viewport: {
+          width: 1440,
+          height: 900
+        },
+        // device scale factor
+        deviceScaleFactor: 1,
+        // is device is mobile
+        isMobile: false,
+        // support of touch events
+        hasTouch: false
+      },
+      {
+        // Name of device
+        name: '1280',
+        // Page width and height
+        viewport: {
+          width: 1280,
+          height: 720
+        },
+        // device scale factor
+        deviceScaleFactor: 1,
+        // is device is mobile
+        isMobile: false,
+        // support of touch events
+        hasTouch: false
+      },
+      {
+        // Name of device
+        name: 'Tablet',
+        // Page width and height
+        viewport: {
+          width: 768,
+          height: 1024
+        },
+        // device scale factor
+        deviceScaleFactor: 1,
+        // is device is mobile
+        isMobile: false,
+        // support of touch events
+        hasTouch: true
+      }
+    ]
+  }
