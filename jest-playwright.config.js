9b608f878bca022ffeb75878b7f1dc63b7088028
Robert <robert.y.mok@gmail.com>
2020-08-30 13:11:15 -0400

    ng serve jest not working with typescript vrtest.ts

--- jest-playwright.config.js
+++ jest-playwright.config.js
@@ -4,10 +4,10 @@ module.exports = {
     },
     contextOptions: {
       ignoreHTTPSErrors: true,
-      viewport: {
-        width: 1920,
-        height: 1080
-      }
+      // viewport: {
+      //   width: 1920,
+      //   height: 1080
+      // }
     },
     browsers: ["chromium", "firefox", "webkit"],
     devices: [
@@ -26,35 +26,35 @@ module.exports = {
         // support of touch events
         hasTouch: false
       },
-      {
-        // Name of device
-        name: '1280',
-        // Page width and height
-        viewport: {
-          width: 1280,
-          height: 720
-        },
-        // device scale factor
-        deviceScaleFactor: 1,
-        // is device is mobile
-        isMobile: false,
-        // support of touch events
-        hasTouch: false
-      },
-      {
-        // Name of device
-        name: 'Tablet',
-        // Page width and height
-        viewport: {
-          width: 768,
-          height: 1024
-        },
-        // device scale factor
-        deviceScaleFactor: 1,
-        // is device is mobile
-        isMobile: false,
-        // support of touch events
-        hasTouch: true
-      }
+      // {
+      //   // Name of device
+      //   name: '1280',
+      //   // Page width and height
+      //   viewport: {
+      //     width: 1280,
+      //     height: 720
+      //   },
+      //   // device scale factor
+      //   deviceScaleFactor: 1,
+      //   // is device is mobile
+      //   isMobile: false,
+      //   // support of touch events
+      //   hasTouch: false
+      // },
+      // {
+      //   // Name of device
+      //   name: 'Tablet',
+      //   // Page width and height
+      //   viewport: {
+      //     width: 768,
+      //     height: 1024
+      //   },
+      //   // device scale factor
+      //   deviceScaleFactor: 1,
+      //   // is device is mobile
+      //   isMobile: false,
+      //   // support of touch events
+      //   hasTouch: true
+      // }
     ]
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  myCookie;
 
   constructor() { }
 
   ngOnInit() {
+    this.myCookie = document.cookie;
   }
 
 }
