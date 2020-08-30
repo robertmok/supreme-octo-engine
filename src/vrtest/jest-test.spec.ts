bd22e629c26d9b1a8d7dbf265a4548211edf113f
Robert <robert.y.mok@gmail.com>
2020-08-29 21:22:42 -0400

    use jest-image-snapshot to compare

--- src/vrtest/jest-test.spec.ts
+++ src/vrtest/jest-test.spec.ts
@@ -1,6 +1,16 @@
 const playwright = require('playwright');
-const compareImages = require('resemblejs/compareImages');
-const fs = require('mz/fs');
+// const compareImages = require('resemblejs/compareImages');
+// const fs = require('mz/fs');
+
+const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
+const customConfig = { threshold: 0 };
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  customDiffConfig: customConfig,
+  noColors: true,
+});
+expect.extend({ toMatchImageSnapshot });
+const fs = require('fs');
+const path = require('path');
 
 const PAGE_URL = 'http://localhost:4200/';
 const cookies = [{
@@ -9,59 +19,59 @@ const cookies = [{
   value: 'woofs',
 }];
 
-async function getDiff(screenshot, ref) {
-  const options = {
-      output: {
-          errorColor: {
-              red: 255,
-              green: 0,
-              blue: 255
-          },
-          errorType: 'movement',
-          transparency: 0.3,
-          largeImageThreshold: 1200,
-          useCrossOrigin: false,
-          outputDiff: true
-      },
-      scaleToSameSize: true,
-      ignore: 'antialiasing'
-  };
+// async function getDiff(screenshot, ref) {
+//   const options = {
+//       output: {
+//           errorColor: {
+//               red: 255,
+//               green: 0,
+//               blue: 255
+//           },
+//           errorType: 'movement',
+//           transparency: 0.3,
+//           largeImageThreshold: 1200,
+//           useCrossOrigin: false,
+//           outputDiff: true
+//       },
+//       scaleToSameSize: true,
+//       ignore: 'antialiasing'
+//   };
 
-  // The parameters can be Node Buffers
-  // data is the same as usual with an additional getBuffer() function
-  const data = await compareImages(
-      await fs.readFile('./src/vrtest/refs/' + ref + '.png'),
-      await fs.readFile('./src/vrtest/screenshots/' + screenshot + '.png'),
-      options
-  );
+//   // The parameters can be Node Buffers
+//   // data is the same as usual with an additional getBuffer() function
+//   const data = await compareImages(
+//       await fs.readFile('./src/vrtest/refs/' + ref + '.png'),
+//       await fs.readFile('./src/vrtest/screenshots/' + screenshot + '.png'),
+//       options
+//   );
 
-  console.log(data);
-  // { isSameDimensions: true,
-  //   dimensionDifference: { width: 0, height: 0 },
-  //   rawMisMatchPercentage: 0,
-  //   misMatchPercentage: '0.00',
-  //   diffBounds: { top: 1024, left: 1280, bottom: 0, right: 0 },
-  //   analysisTime: 93,
-  //   getImageDataUrl: [Function],
-  //   getBuffer: [Function] }
+//   console.log(data);
+//   // { isSameDimensions: true,
+//   //   dimensionDifference: { width: 0, height: 0 },
+//   //   rawMisMatchPercentage: 0,
+//   //   misMatchPercentage: '0.00',
+//   //   diffBounds: { top: 1024, left: 1280, bottom: 0, right: 0 },
+//   //   analysisTime: 93,
+//   //   getImageDataUrl: [Function],
+//   //   getBuffer: [Function] }
 
-  await fs.writeFile('./src/vrtest/diffs/diff-' + screenshot + '.png', data.getBuffer());
+//   await fs.writeFile('./src/vrtest/diffs/diff-' + screenshot + '.png', data.getBuffer());
 
-  return analyzeResult(data);
-}
+//   return analyzeResult(data);
+// }
 
-function analyzeResult(data) {
-  if (data.isSameDimensions &&
-    data.rawMisMatchPercentage === 0 &&
-    data.misMatchPercentage === '0.00'
-  ) {
-    return true;
-  }
-  return false;
-}
+// function analyzeResult(data) {
+//   if (data.isSameDimensions &&
+//     data.rawMisMatchPercentage === 0 &&
+//     data.misMatchPercentage === '0.00'
+//   ) {
+//     return true;
+//   }
+//   return false;
+// }
 
 // Loop over all the supported browsers
-for (const browserType of ['chromium', 'firefox', 'webkit']) {
+for (const browserType of ['chromium']) { // 'firefox', 'webkit'
 
   describe(`(${browserType}): Jest UI Tests with Playwright`, () => {
     let browser = null;
@@ -102,14 +112,24 @@ for (const browserType of ['chromium', 'firefox', 'webkit']) {
       expect(page).not.toBeNull();
       expect(await page.title()).not.toBeNull();
 
-      let screenshotName = browserType + '-' + ticks;
-      await page.screenshot({ path: './src/vrtest/screenshots/' + screenshotName + '.png' });
+      // let screenshotName = browserType + '-' + ticks;
+      // await page.screenshot({ path: './src/vrtest/screenshots/' + screenshotName + '.png' });
 
       const loadedCookies = await context.cookies('http://localhost:4200/');
       console.log(JSON.stringify(loadedCookies, null, 4));
 
-      let comparedResult = await getDiff(screenshotName, browserType);
-      expect(comparedResult).toEqual(true);
+      // let comparedResult = await getDiff(screenshotName, browserType);
+      // expect(comparedResult).toEqual(true);
+
+      // const imageAtTestPath = path.resolve(__dirname, './refs', browserType + '.png');
+      // // imageAtTest is a PNG encoded image buffer which is what `toMatchImageSnapshot() expects
+      // const imageAtTest = fs.readFileSync(imageAtTestPath);
+      // expect(imageAtTest).toMatchImageSnapshot();
+
+      // jest-image-snapshot automatically keeps first screenshot and compares on subsequent runs
+      // https://codeburst.io/automatic-visual-regression-testing-23cc06471dd
+      let testScreenshot = await page.screenshot();
+      expect(testScreenshot).toMatchImageSnapshot();
     });
 
   });
